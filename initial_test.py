9f05fb6cf495620ee8f2f46cfa950c2772267d6c
Kuleen Sasse <56900136+KuleenS@users.noreply.github.com>
2025-11-17 11:02:14 -0500

    Refactor initial_test.py for improved functionality

--- initial_test.py
+++ initial_test.py
@@ -2,18 +2,24 @@ import argparse
 import pickle
 
 import torch
+
 import transformers
+
+from tqdm import tqdm
+import json
 
 def main(args):
 
     model_id = args.model_id
 
-    model_kwargs = {"torch_dtype": torch.bfloat16, "device_map": "auto"}
+    model_kwargs = {"dtype": torch.bfloat16, "device_map": "auto"}
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
     with open(args.data, "rb") as f:
         data = pickle.load(f)
+
+    data = data[args.starting_index:100]
 
     for item in data:
         item["legal_moves"] = [str(x) for x in item["legal_moves"]]
@@ -48,14 +54,23 @@ def main(args):
         **model_kwargs
     )
 
-    outputs = pipeline(messages)
+    for i in tqdm(range(0, len(messages), args.batch_size), desc="Generating text"):
+        batch = messages[i:i + args.batch_size]
+        
+        outputs = pipeline(batch)
 
-    print(outputs)
+        for j, out in enumerate(outputs):
+            with open(f"outputs/initial_test_outputs_{model_id.replace('/', '_')}_{args.starting_index+i+j}.json", "w") as f:
+                f.write(json.dumps(out))
+            
+
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_id')
     parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--starting_index', type=int, default=0)
     parser.add_argument('--data')
     args = parser.parse_args()
 
